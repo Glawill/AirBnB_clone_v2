68884bdb44e9296c142081d7b6b14b8b12ef2b9c
Glawill <hulukile@gmail.com>
2024-04-08 08:21:47 -0700

    0x03.AirBnB

---
+++ 100-clean_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# script that deletes out-of-date archives, using the function do_clean
+
+from fabric.api import env, run, local
+import os
+
+env.hosts = ['54.90.14.86', '54.160.114.180']
+
+
+def do_clean(number=0):
+        """
+            deletes out-of-date archives
+                """
+                    number = int(number)
+
+                        if number == 0 or number == 1:
+                                    number = 1
+                                        else:
+                                                    number += 1
+
+                                                        local("ls -1t versions/ | tail -n +{} | \
+                                                                            xargs -I {{}} rm versions/{{}}".format(number))
+                                                                                run("ls -1t /data/web_static/releases/ | tail -n +{} | \
+                                                                                                    xargs -I {{}} rm -rf /data/web_static/releases/{{}}".format(
+                                                                                                                    number))
